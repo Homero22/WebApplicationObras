52fe49101266d2aabf47c2eb9a4e2223bf1eaa1e
Homero <homero6834@gmail.com>
2024-09-05 12:00:03 -0500

    Agregar archivos de proyecto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationObras.Models;
+
+namespace WebApplicationObras.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+        {
+            
+        }
+
+ 
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplicationObras.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; }
+        = string.Empty; 
+
+
+    }
+}
